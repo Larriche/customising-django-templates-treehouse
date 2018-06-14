47aa2cdd897cf38fcbf34cfc7941dba2b9360a49
Richman Larry Clifford <larriche99@gmail.com>
2018-06-14 04:24:04 +0000

    Tried out a couple more filters

--- courses/views.py
+++ courses/views.py
@@ -5,7 +5,8 @@ from .models import Course, Step
 
 def course_list(request):
     courses = Course.objects.all()
-    return render(request, 'courses/course_list.html', {'courses': courses})
+    email = 'questions@learningsite.com'
+    return render(request, 'courses/course_list.html', {'courses': courses, 'email': email})
 
 
 def course_detail(request, pk):
