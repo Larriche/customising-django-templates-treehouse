be57cec2b3f526e14fb4a0d20c81d4a2b2928060
Richman Larry Clifford <larriche99@gmail.com>
2018-06-16 23:13:40 +0000

    Added code demonstrating creating tags that make use of templates

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -7,3 +7,9 @@ register = template.Library()
 def newest_course():
     '''Gets the most recent course that was added to the library'''
     return Course.objects.latest('created_at')
+
+@register.inclusion_tag('courses/course_nav.html')
+def nav_courses_list():
+    '''Return dictionary of courses to display as a navigation pane'''
+    courses = Course.objects.all()
+    return {'courses': courses}
