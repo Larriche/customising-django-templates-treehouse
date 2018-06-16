fb61b91cf5164cef0d4eeccba4574dbb1e4bb616
Richman Larry Clifford <larriche99@gmail.com>
2018-06-16 22:56:28 +0000

    Fixes to custom tag for getting latest course

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -4,6 +4,6 @@ from courses.models import Course
 register = template.Library()
 
 @register.simple_tag
-def newest_courses():
-    '''Gets the most recent courses that were added to the library'''
-    Course.objects.latest('created_at')
+def newest_course():
+    '''Gets the most recent course that was added to the library'''
+    return Course.objects.latest('created_at')
