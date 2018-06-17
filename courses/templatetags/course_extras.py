96c95607044e109a3ba348b020002d663191bc4d
Richman Larry Clifford <larriche99@gmail.com>
2018-06-17 00:04:52 +0000

    Added codes for markdown filter example

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -1,5 +1,8 @@
 from django import template
 from courses.models import Course
+from django.utils.safestring import mark_safe
+
+import markdown2
 
 register = template.Library()
 
@@ -13,3 +16,16 @@ def nav_courses_list():
     '''Return dictionary of courses to display as a navigation pane'''
     courses = Course.objects.all()
     return {'courses': courses}
+
+@register.filter('time_estimate')
+def time_estimate(word_count):
+    '''Return an estimated duration for a course step based on content length'''
+    minutes = round(word_count / 20)
+    return minutes
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+    '''Converts markdown text to HTML'''
+    html_body = markdown2.markdown(markdown_text)
+    return mark_safe(html_body)
+
